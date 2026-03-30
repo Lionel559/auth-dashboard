b0ab97acc01e976fd9e65eaadfb0bb5355c9a727
Lionel <ayooladeji8@gmail.com>
2026-03-30 13:00:05 +0100

    render port fix

--- backend/server.js
+++ backend/server.js
@@ -129,6 +129,8 @@ res.json({message:"User updated"})
 
 })
 
-app.listen(5000,()=>{
-console.log("Server running on port 5000")
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+console.log(`Server running on port ${PORT}`)
 })
